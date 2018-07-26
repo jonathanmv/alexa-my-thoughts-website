69a89d61ba2c162ebe39f6a51bd36c0bdee3d9da
Jonathan Morales Vélez <jonathan@themidgame.com>
2018-07-27 00:06:19 +0100

    Changed config to production

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   LOGIN_WITH_AMAZON_CLIENT_ID: '"amzn1.application-oa2-client.3e78ebeecc044a879492e687f71ecc12"',
-  LOGIN_WITH_AMAZON_REDIRECT_URL: '"http://localhost:8080"'
+  LOGIN_WITH_AMAZON_REDIRECT_URL: '"https://jonathanmv.github.io/alexa-my-thoughts-website/"'
 }
