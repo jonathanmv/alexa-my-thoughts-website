638a23d4c7ae64691e6ccf0376774dcd12df80b9
Jonathan Morales Vélez <jonathan@themidgame.com>
2018-08-08 13:32:27 +0100

    Added cognito user pool env variables

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  LOGIN_WITH_AMAZON_REDIRECT_URL: '"http://localhost:8080/"'
+  LOGIN_WITH_AMAZON_REDIRECT_URL: '"http://localhost:8080/"',
+  USER_POOL_ID: '"us-east-1_d1oxqzK3J"',
+  USER_POOL_CLIENT_ID: '"2ipmplpov1351c65brmafe22lo"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -3,5 +3,7 @@ module.exports = {
   NODE_ENV: '"production"',
   LOGIN_WITH_AMAZON_CLIENT_ID: '"amzn1.application-oa2-client.3e78ebeecc044a879492e687f71ecc12"',
   LOGIN_WITH_AMAZON_REDIRECT_URL: '"https://jonathanmv.github.io/alexa-my-thoughts-website/"',
-  BACKEND_URL: '"https://6vl99zf778.execute-api.us-east-1.amazonaws.com/dev"'
+  BACKEND_URL: '"https://6vl99zf778.execute-api.us-east-1.amazonaws.com/dev"',
+  USER_POOL_ID: '"us-east-1_8Uqm0Rk8z"',
+  USER_POOL_CLIENT_ID: '"3chmsu1pshm2vc1jif0s65gtvr"'
 }
